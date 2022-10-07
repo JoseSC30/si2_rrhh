4a77fcd10a24a8627aea0712561d7a50b9c100eb
joseluis <joseluis.universidad2020@gmail.com>
2022-10-07 14:44:26 -0400

    prueba a parte 11

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,5 @@ Route::resource('chau1', App\Http\Controllers\UserController::class);
 Route::resource('chau3', App\Http\Controllers\UserController::class);
 Route::resource('chau4', App\Http\Controllers\UserController::class);
 Route::resource('chau2', App\Http\Controllers\UserController::class);
+Route::resource('chau7', App\Http\Controllers\UserController::class);
+Route::resource('chau8', App\Http\Controllers\UserController::class);
