268d8c430fc636c5d6d9bb23cd8c3d04346614a1
Edith008 <edith.sossa.c98@gmail.com>
2022-10-07 14:26:51 -0400

    prueba a parte 6

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,4 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::resource('rols', App\Http\Controllers\RolController::class);
 Route::resource('empleados', App\Http\Controllers\EmpleadoController::class);
 Route::resource('users', App\Http\Controllers\UserController::class);
+Route::resource('chau1', App\Http\Controllers\UserController::class);
